e44e1c48409250e0a8db3ecd7cac8721608df647
Tobias Reinecke <tobias.reinecke2@student.uni-halle.de>
2024-10-29 20:21:07 +0100

    Main.js aufgeräumt

--- app/client/main.js
+++ app/client/main.js
@@ -1,45 +1,20 @@
 "use strict"
 
-const ClientGame = require("./clientgame")
-let clientGame = new ClientGame()
-clientGame.openWebSocket()
-
-//Beispiel: Zeichnen auf Board
-const Message = require('../client/class/message');
-const Action = require('../client/class/action');
-
-let action = new Action('pen', 3 , 4, '#000000', 1);
-let message = new Message('action', action);
-
-//setTimeout(() => clientGame.send(JSON.stringify(message)), 1000);
-
-setTimeout(() => clientGame.sendDrawAction('pen', 1, 2, '#000000', 2), 1000);
-
-//setTimeout(() => clientGame.send(JSON.stringify(message)), 1000);
-
-//setTimeout(() => clientGame.send(JSON.stringify(message)), 2000);
-
-//setTimeout(() => clientGame.sendDrawAction('pen', 3, 4, '#000000', 2), 1000);
-
-/*
-
-"use strict"
-
-const ClientGame = require("./clientgame")
+const ClientGame = require("./clientgame");
 
 let clientGame = new ClientGame();
 clientGame.openWebSocket();
+
+//Hier sind Beispiel-Actions, die der Client an den Server schickt --> so einfach über das Canvas auslösen
 
 //Beispiel: Zeichnen auf Board --> clientGame.sendDrawAction() mit Tool pen
 setTimeout(() => clientGame.sendDrawAction('pen', 3, 4, '#000000', 3), 1000);
 
 //Beispiel: Radieren auf Board --> clientGame.sendDrawAction() mit Tool eraser
-//setTimeout(() => clientGame.sendDrawAction('eraser', 3, 4, '', 3), 2000);
+setTimeout(() => clientGame.sendDrawAction('eraser', 3, 4, '', 3), 2000);
 
 //Beispiel: Füllen des Boards mit Farbe --> clientGame.sendFillAction()
-//setTimeout(() => clientGame.sendFillAction('#888888'), 3000);
+setTimeout(() => clientGame.sendFillAction('#888888'), 3000);
 
 //Beispiel: Kompletter Clear auf Board --> clientGame.sendClearAction()
-//setTimeout(() => clientGame.sendClearAction(), 4000);
-
-*/
+setTimeout(() => clientGame.sendClearAction(), 4000);
