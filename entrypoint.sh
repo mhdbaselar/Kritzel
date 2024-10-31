b674bfaef67162a89f7b01bedc05af535e119f38
Hans Eckardt <hans.eckardt@student.uni-halle.de>
2024-10-31 17:38:22 +0000

    Revert "Merge branch '17-story-radierer-und-bucket-tool' into 'main'"
    This reverts merge request !7

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ cd /app
 # Install dependencies
 npm install
 
-# Start watchify to continuously re-bundle main.js
-watchify ./client/main.js -o ./public/bundle.js -v &
+# Start browserify in watch mode to re-bundle front-end files on change
+browserify ./client/main.js -o ./public/bundle.js -w -v &
 
 # Start nodemon to monitor back-end changes and restart the server as needed
 nodemon --watch ./ --exec "node ./kritzel.js"
