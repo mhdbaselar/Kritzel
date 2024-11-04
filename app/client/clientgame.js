d304e22564b9e35c929b9b0799e42b4f1137e8b7
Hans Eckardt <hans.eckardt@student.uni-halle.de>
2024-11-04 08:28:18 +0100

    fix moiré patterns on fill

--- app/client/clientgame.js
+++ app/client/clientgame.js
@@ -231,6 +231,7 @@ module.exports = class ClientGame {
         if (tool === 'pen') _tool = 'pen';
         if (tool === 'eraser') _tool = 'eraser';
         if (tool === 'fill') _tool = 'fill';
+        if (tool === 'fill') _tool = 'fill';
 
         let action = new Action(_tool, x, y, color, thickness);
         let message = new Message('action', action);
@@ -238,6 +239,9 @@ module.exports = class ClientGame {
         let _message = JSON.stringify(message);
 
         this.send(_message);
+        if (tool === 'fill') {
+            setTimeout(this.sendGetCanvasAction(), 500); // Get new canvas on fill, to fix Moiré patterns
+        }
     }
 
     /**
