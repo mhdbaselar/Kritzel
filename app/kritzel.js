f09fd0498f7a12197a71ad72f522672bba8abeaf
Hans Eckardt <hans.eckardt@student.uni-halle.de>
2024-11-07 23:37:17 +0100

    Removed Canvas resend every 2 sec due to ws package using TCP

--- app/kritzel.js
+++ app/kritzel.js
@@ -1,35 +1,38 @@
-"use strict"
+"use strict";
 
-const TinyServer = require('./server/tinyserver');
-const ServerGame = require('./server/servergame');
+const TinyServer = require("./server/tinyserver");
+const ServerGame = require("./server/servergame");
 
 // Creates a Server (organizes WebSocketServer)
 let server = new TinyServer(8123, (uid, data) => {
-    receive(uid, data);
+  receive(uid, data);
 });
 
 let refreshCounter = 0;
 
 // Creates a ServerGame (organizes game logic server-side)
 let game = new ServerGame(server, () => {
-    let data;
-    let type;
-    
-    if(game.getIsSendPointList() && refreshCounter < 20){          // send point list
-        data = game.getBoard().getPoints();
-        type = "pl";
-    } else{                                 // send 2D-array (board)
-        data = game.getBoard().getBoard(); 
-        type = "2d";
-        refreshCounter = 0;
-    }
+  let data;
+  let type;
 
-    refreshCounter++;
+  if (game.getIsSendPointList()) {
+    // removed && refreshCounter < 20
+    // send point list
+    data = game.getBoard().getPoints();
+    type = "pl";
+  } else {
+    // send 2D-array (board)
+    data = game.getBoard().getBoard();
+    type = "2d";
+    refreshCounter = 0;
+  }
 
-    let json = JSON.stringify({type : type , data : data});
-    game.getBoard().setPointsEmpty();
-    game.setIsSendPointList(true);
-    server.broadcastWsMessage(null, json, false, 'all');
+  refreshCounter++;
+
+  let json = JSON.stringify({ type: type, data: data });
+  game.getBoard().setPointsEmpty();
+  game.setIsSendPointList(true);
+  server.broadcastWsMessage(null, json, false, "all");
 });
 
 game.start();
@@ -39,11 +42,11 @@ game.start();
  * @param {string} uid user unique ID
  * @param {Message} message client request
  */
-function receive(uid ,message){
-    game.processInput(uid, message);
+function receive(uid, message) {
+  game.processInput(uid, message);
 }
 
 // catch all unhandled exceptions
-process.on('uncaughtException', (err) => {
-    console.log(err);
+process.on("uncaughtException", (err) => {
+  console.log(err);
 });
