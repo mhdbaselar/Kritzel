8604befe783f3bf296eb2203cdb164e85bb6efa5
Yanik Engelhardt <yanik.ye@gmail.com>
2024-12-16 19:38:23 +0100

    lobby join buttons oberhalb ausgerichtet

--- app/client/main.js
+++ app/client/main.js
@@ -572,10 +572,10 @@ function hideCreateLobby() {
   // Versteckt das Lobby-Einstellungs-Overlay
   document.getElementById("lobbyCreate").style.display = "none";
 }
+//Leave Lobby Button
 window.leaveLobbyAndShowMenu = function () {
-  // Danach Modal schließen und Lobby-Einstellungen anzeigen
-  //document.getElementById("usernameModal").style.display = "none"; showLobbyMenu();
-  //clientGame.sendLeaveLobbyAction();
+  showLobbyMenu();
+  clientGame.sendLeaveLobbyAction();
   console.log("Lobby verlassen und Lobby-Menü angezeigt");
 };
 window.changeLanguage = function (language) {
