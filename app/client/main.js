53a20983c546e9a12e6b9509680a4a04a6dfa59d
Yanik Engelhardt <yanik.ye@gmail.com>
2024-12-19 14:16:28 +0100

    Lobby-List visuell angepasst

--- app/client/main.js
+++ app/client/main.js
@@ -677,19 +677,19 @@ window.displayLobbyList = function (lobbyArray) {
   let html = "";
   lobbyArray.forEach((lobby) => {
     html += `
-      <div class="lobby-item" style="border: 1px solid #ccc; margin-bottom:10px; padding:10px;">
+      <div class="lobby-item">
         <h3>Lobby: ${lobby.lobbyName} ${lobby.isPublic ? "🌐" : "🔒"}</h3>
         <p>Spieler: ${lobby.currentPlayers}/${lobby.maxPlayers}</p>
         ${
           lobby.isPublic
             ? ""
-            : `<input id="codeInputField${lobby.lobbyID}" type="text">`
+            : `<input id="codeInputField${lobby.lobbyID}" type="text" placeholder="Lobby-Code">`
         }
-        <button onclick="joinThisLobby(${lobby.lobbyID},${
+        <button onclick="joinThisLobby(${lobby.lobbyID}, ${
       lobby.isPublic
     })">Beitreten</button>
       </div>
     `;
   });
-  lobbyListContainer.innerHTML = html;
+  document.getElementById("lobbyListContainer").innerHTML = html;
 };
